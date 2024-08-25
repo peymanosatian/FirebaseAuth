9ef54bab80ac78c1c69164fbfebd576a16adaf24
Peyman <peyman.osatian@gmail.com>
2024-08-25 10:52:45 +0200

    AuthUI

---
+++ FirebaseAuth/Core/Authentication/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  FirebaseAuth
+//
+//  Created by Peyman Osatian on 2024-08-25.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
